f074f8332827d6873fe8f6683cf70fac582327ef
Paynecoder <joshpayne03@icloud.com>
2024-09-09 23:28:30 -0700

    learn more and styling

--- GDSWeb/src/app/components/navbar/navbar.component.ts
+++ GDSWeb/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
