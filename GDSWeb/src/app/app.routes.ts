afc096c6bca38c899b4a1e5909d022c3eb7844d6
paynecoder <joshpayne03@icloud.com>
2024-09-09 22:16:17 -0700

    init

---
+++ GDSWeb/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FormComponent } from './components/form/form.component';
+import { ResultsComponent } from './components/results/results.component';
+import { LearnComponent } from './components/learn/learn.component';
+
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'learn', component: LearnComponent }
+];
