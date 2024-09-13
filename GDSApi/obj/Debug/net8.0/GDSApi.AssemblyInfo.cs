ab9236e55a39d07d9db1e7330dce0660fef19e93
Paynecoder <joshpayne03@icloud.com>
2024-09-12 21:48:00 -0700

    comment

--- GDSApi/obj/Debug/net8.0/GDSApi.AssemblyInfo.cs
+++ GDSApi/obj/Debug/net8.0/GDSApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GDSApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6480b1680ed920db7d7e32b84db6f4b9406a509f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b7057670eb1cc85f391cb5e24a60289ac87bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GDSApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GDSApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
